cbc66fc54c622dfee880b0406d1e26408937e119
Fahad Hossain <fahadhossain@Macmini.lan>
2025-02-09 19:03:58 -0500

    Add GitHub Pages configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['placehold.co'],
+    unoptimized: true,
   },
+  basePath: '/hearthandhamper',
+  assetPrefix: '/hearthandhamper/',
 }
 
 module.exports = nextConfig 
